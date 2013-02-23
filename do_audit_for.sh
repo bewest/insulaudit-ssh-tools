880d05b3152aee24e2898338311793d929f5b8f0
Ben West <bewest@gmail.com>
2013-02-23 23:46:54 +0000

    stub out basic workflow

--- do_audit_for.sh
+++ do_audit_for.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-key=${1}
+export AUDIT_USER=${1}
 
 echo \$0: $0
-echo key: $key
-echo args: $*
-echo args: $@
-echo ENV
+
+# parse SSH_ORIGINAL_COMMAND to figure out what the user wanted to do
+read tool session user <<< "$SSH_ORIGINAL_COMMAND"
+
+echo tool: $tool
+echo session: $session
+echo user: $user
+export AUDIT_TOOL=$tool
+export AUDIT_SESSION=$session
+export AUDIT_UID=$user
+
+export PATH="~/bin:$PATH"
 env
+if [[ $AUDIT_TOOL == "audit" ]] ; then
+  setup_audit_work
+  perform_audit
+  clean_audit_work
+fi
 echo "THIS IS MY SPECIAL LOGIN SHELL. HANGING UP."
 
 #####
