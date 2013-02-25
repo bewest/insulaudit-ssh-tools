1a9637c87c8763a93fd76a7317adc84eac35316c
Ben West <bewest@gmail.com>
2013-02-25 08:14:35 +0000

    automatically check medical records into git
    WOW

--- do_audit_for.sh
+++ do_audit_for.sh
@@ -18,29 +18,35 @@ export AUDIT_UID=${user}
 
 export BASE=$HOME/session
 
-export WORK_BASE="$HOME/sessions/${user}-${session}"
+export SESS_KEY="${user}-${session}"
+export BRANCH="audit/$(date +%F)-${SESS_KEY}"
+export WORK_BASE="$HOME/sessions/${SESS_KEY}"
 export WORK_CONF=$WORK_BASE/conf
-export WORK_REMOTE=$WORK_BASE/remote
+export WORK_REMOTE=$WORK_BASE/remote.git
 export WORK_LOCAL=$WORK_BASE/local
 export VMODEM=${WORK_BASE}/vmodem
 
 export PATH="$HOME/bin:$PATH"
 export WORK_BASE WORK_CONF WORK_REMOTE WORK_LOCAL BASE VMODEM
-env
+
+SESSION_CONFIG=$WORK_CONF/audit.config
+export SESSION_CONFIG BRANCH SESS_KEY
+
+. common
+
 if [[ $AUDIT_TOOL == "audit" ]] ; then
-  . setup_audit_work
+  PHR_REPO=$(read_config registration.phr)
+  export PHR_REPO
+  env
+  setup_audit_work
   echo "CREATE VMODEM" $(which create_vmodem)
   create_vmodem $WORK_CONF/audit.config
   sleep 2
-  . perform_audit
-  . clean_audit_work
+  perform_audit
+  clean_audit_work
 fi
 ps
 echo "THIS IS MY SPECIAL LOGIN SHELL. HANGING UP."
-
-#echo $PPID
-#kill -9 $PPID
-
 
 #####
 # EOF
